e43fa102d805249bbb689878cfe209e1c2083fcd
Allanlimo02 <allan.limo@student.moringaschool.com>
2021-07-09 22:24:05 +0300

    Validated contact us form

--- validator.js
+++ validator.js
@@ -1,6 +1,6 @@
 function validator(){
-    username=document.getElementById('username').value;
-    useremail=document.getElementById('useremail').value;
+    username=document.getElementById('username');
+    useremail=document.getElementById('useremail');
     if(username==''){
         alert('Dear user, kindly input your Name');
         username.focus();
@@ -11,6 +11,6 @@ function validator(){
         useremail.focus();
         return validator;
     }
-    alert('Dear '+username.value+' I have receive your email\I will get back to you soon.\n Thanks')
+    alert('Dear '+username.value +' I have receive your email\I will get back to you soon.\n Thanks')
 
 }
