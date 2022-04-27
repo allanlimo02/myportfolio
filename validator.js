643975041832a327acf893c7850f4e4bf29862bf
allanlimo02 <allanlimo02@gmail.com>
2022-04-27 22:16:47 +0300

    April 2022 updates

--- validator.js
+++ validator.js
@@ -1,16 +1,19 @@
+username=document.getElementById('username');
+useremail=document.getElementById('useremail');
 function validator(){
-    username=document.getElementById('username');
-    useremail=document.getElementById('useremail');
     if(username==''){
         alert('Dear user, kindly input your Name');
         username.focus();
         return validator;
-    }
-    if (useremail==''){
+    }else
+    if (useremail.equals('')){
         alert('Dear '+username.value+' KIndly input your email')
         useremail.focus();
         return validator;
+    }else{
+        alert('Dear '+username.value +' I have receive your email\I will get back to you soon.\n Thanks')
+        return validator;
     }
-    alert('Dear '+username.value +' I have receive your email\I will get back to you soon.\n Thanks')
+    
 
 }
